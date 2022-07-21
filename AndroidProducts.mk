e05cd07c6f25639269dd7cb6d11dc6a36c4c0a55
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-07-21 12:03:06 +0200

    b2q: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: samsung Model: SM-F711N Script version: 2.3.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_b2q.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_b2q-user \
+    omni_b2q-userdebug \
+    omni_b2q-eng
